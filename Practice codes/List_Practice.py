24c78f3d4f89155ee6bca9f0d51e520d68db2f05
Aditya Puranik <puranik.aditya.ap@gmail.com>
2023-09-19 01:03:18 -0400

    Python String & List practice

---
+++ Practice codes/List_Practice.py
@@ -0,0 +1,47 @@
+mylist = ["apple", "banana", "cherry", "apple", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+#Accessing
+# print(mylist[2:5])
+# print(mylist[:5])
+# print(mylist[2:])
+# print(mylist[-4:-1])
+
+#Adding value to list at any index
+# print(mylist.insert(3,"Mango"))
+# # print(mylist)
+
+#Adding at last in list
+# print(mylist.append("grapes"))
+# print(mylist)
+
+#Extend list
+# mylist.extend(tropical)
+# print(mylist)
+
+#Remove from list
+# mylist.remove("apple")  #remove by value
+# print(mylist)
+# mylist.pop(3)    # remove by index
+# print(mylist)
+
+#Delete value from list
+# del mylist[0]
+# print(mylist)
+
+#Empty the list
+# mylist.clear()
+# print(mylist)
+
+#Sort list
+# mylist.sort()
+# print(mylist)
+
+#Copy list
+# newlist = mylist.copy()
+# print(newlist)
+
+
+
+
+
+
---
+++ Practice codes/String_Practice.py
@@ -0,0 +1,25 @@
+str = " My name is Aditya Puranik Aditya "
+
+#Slice
+# print(str[3:7])
+# print(str[:7])
+# print(str[3:])
+# print(str[-8:-3])
+
+#modify
+# print(str.lower())
+# print(str.upper())
+# print(str.capitalize())
+# print(str.replace(" ",""))
+# print(str.strip())
+# print(str.lstrip())
+# print(str.rstrip())
+# print(str.split(" "))
+# a= str.split(" ")
+# print(type(a))
+# print(str[::-1])
+# txt = "We are the so-called \"Vikings\" from the north."
+
+
+
+
