990dcbdd58f2b41c592826bae15b23da6e16116d
Aditya Puranik <puranik.aditya.ap@gmail.com>
2023-09-20 00:37:53 -0400

    tuple and set practice

---
+++ Practice codes/Set_Practice.py
@@ -0,0 +1,27 @@
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+# print(type(set1))
+# print(set1)
+# print(set2)
+# print(set3)
+
+# print("banana" in set1)
+
+# for x in set2:
+#     print(x)
+
+#Add in set
+
+# set1.add("Mango")
+# print(set1)
+
+
+# To add items from another set into the current set, use the update() method.
+
+# set1.update(set2)
+# print(set1)
+
+# Remove item from set
+# set1.remove("cherry")
+# print(set1)
---
+++ Practice codes/Tuple_Practice.py
@@ -0,0 +1,13 @@
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+
+# print(thistuple)
+
+# Access tuple
+# print(thistuple[1:4])
+# print(thistuple[:4])
+# print(thistuple[1:])
+# print(thistuple[3])
+
+
+
+
