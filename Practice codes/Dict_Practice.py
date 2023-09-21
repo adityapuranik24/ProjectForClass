44e17c4097d7f34aa036d4fe61fe35aa662b5e94
Aditya Puranik <puranik.aditya.ap@gmail.com>
2023-09-20 23:50:20 -0400

    Dict_Practice

---
+++ Practice codes/Dict_Practice.py
@@ -0,0 +1,36 @@
+dict = {'Name': 'Geeks', 1: [1, 2, 3, 4]}
+
+# print(dict)
+
+#Adding value in dict
+
+# dict["Marks"] = [45, 65,69,55]  # multiple values
+# dict[3] = "Apple"  # one value at a time
+# print(dict)
+# dict.update(Name="Adityas")
+# print(dict)
+
+#Accessing dict value by key
+# print(dict["Name"])
+# print(dict[1])
+
+#Accessing nested value of dict
+# print(dict[1][3])
+
+#Deleting key of a dict
+
+# del(dict["Name"])
+# print(dict)
+
+# a = dict.pop("Name")
+# print(a)
+#
+# dic.clear()	Remove all the elements from the dictionary
+# dict.copy()	Returns a copy of the dictionary
+# dict.get(key, default = “None”)	 Returns the value of specified key
+# dict.items()	 Returns a list containing a tuple for each key value pair
+# dict.keys()	 Returns a list containing dictionary’s keys
+# dict.update(dict2)	Updates dictionary with specified key-value pairs
+# dict.values()	 Returns a list of all the values of dictionary
+# pop()	 Remove the element with specified key
+# popItem()	Removes the last inserted key-value pair
